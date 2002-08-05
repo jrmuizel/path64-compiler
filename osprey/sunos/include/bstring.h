9beaf3f2d5040e6573264e22f0ef6385714f1c0e
zoryj <zoryj@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2002-08-05 14:26:19 +0000

    changed host specific include mechanism
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@960 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/sunos/include/bstring.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __BSTRING_H__
-#define __BSTRING_H__
-
-#ifndef _STRINGS_H 
-#include_next <strings.h>
-#endif
-
-#endif /* !__BSTRING_H__ */
--- osprey/sunos/include/errno.h
+++
@@ -1,7 +0,0 @@
-
-extern char* sys_errlist[];
-extern int sys_nerr;
-
-#ifndef _ERRNO_H
-#include_next<errno.h>
-#endif
