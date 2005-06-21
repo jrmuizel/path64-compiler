e893cf7915d37cc57d2ded1f6f28eb9efd4c8b3b
lyon <lyon@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2005-06-21 14:09:11 +0000

    [CL] debug_frame: fix prototype of Dwarf_P_Fde Build_Fde_For_Proc()        for additional scn_index param and type of begin/end_label      added after_op param to Emit_Unwind_Directives_For_OP() to allow      for pre and post operator/bundle emission
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@6361 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/lai/cgdwarf_targ.h
+++ osprey/lai/cgdwarf_targ.h
@@ -39,6 +39,24 @@
 extern BOOL Trace_Dwarf;
 
 /* construct the fde for the current procedure. */
+#ifdef TARG_ST
+// [CL] changed begin/end_label type and added
+// scn_index param to be used by the debug_frame
+// machinery along with begin_label
+extern Dwarf_P_Fde Build_Fde_For_Proc (Dwarf_P_Debug  dw_dbg,
+				       BB            *firstbb,
+				       Dwarf_Unsigned begin_label,
+				       Dwarf_Unsigned end_label,
+				       INT32          end_offset,
+				       // The following two arguments
+				       // need to go away once
+				       // libunwind gives us an
+				       // interface that supports
+				       // symbolic ranges.
+				       INT       low_pc,
+				       INT       high_pc,
+				       Elf64_Word	scn_index);
+#else
 extern Dwarf_P_Fde Build_Fde_For_Proc (Dwarf_P_Debug  dw_dbg,
 				       BB            *firstbb,
 				       LABEL_IDX      begin_label,
@@ -51,6 +69,7 @@ extern Dwarf_P_Fde Build_Fde_For_Proc (Dwarf_P_Debug  dw_dbg,
 				       // symbolic ranges.
 				       INT       low_pc,
 				       INT       high_pc);
+#endif
 
 extern void Check_Dwarf_Rel(const Elf32_Rel &);
 extern void Check_Dwarf_Rel(const Elf64_Rel &);
@@ -60,6 +79,11 @@ extern BOOL Is_Dwarf_Section_To_Emit(const char *name);
 
 extern void Init_Unwind_Info (BOOL trace);
 extern void Finalize_Unwind_Info(void);
+#ifdef TARG_ST
+// [CL] need to emit labels after bundles too
+extern void Emit_Unwind_Directives_For_OP(OP *op, FILE *f, BOOL after_op=FALSE);
+#else
 extern void Emit_Unwind_Directives_For_OP(OP *op, FILE *f);
+#endif
 
 #endif /* cgdwarf_targ_INCLUDED */
