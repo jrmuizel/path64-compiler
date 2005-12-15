439a5a690520c5ceb1cf69c55e1a4dbb8d7f7e7d
bruel <bruel@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2005-12-15 16:49:13 +0000

    disable emission of dwarf infos for restore for .eh_frame
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@7520 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/lai/cgdwarf_targ.h
+++ osprey/lai/cgdwarf_targ.h
@@ -56,7 +56,8 @@ extern Dwarf_P_Fde Build_Fde_For_Proc (Dwarf_P_Debug  dw_dbg,
 				       // symbolic ranges.
 				       INT       low_pc,
 				       INT       high_pc,
-				       Elf64_Word	scn_index);
+				       Elf64_Word	scn_index,
+				       bool emit_restores=false);
 #else
 extern Dwarf_P_Fde Build_Fde_For_Proc (Dwarf_P_Debug  dw_dbg,
 				       BB            *firstbb,
