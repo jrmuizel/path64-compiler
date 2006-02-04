596e3e4816fd2e4efede962ff222fd49ddf893a1
guillon <guillon@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2006-02-04 03:27:33 +0000

    [trunk/osprey/common] Remove useless files.
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@7857 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/common/util/util/be_export.h
+++
@@ -1,9 +0,0 @@
-// [HK] defines BE_EXPORTED_CLASS and BE_EXPORTED_METHOD depending on the version of GCC, to workaround an uncompatibility between gcc-2.95 and gcc-3.3
-
-#if __GNUC__ >=3
-#define BE_EXPORTED_CLASS BE_EXPORTED
-#define BE_EXPORTED_METHOD
-#else
-#define BE_EXPORTED_CLASS 
-#define BE_EXPORTED_METHOD BE_EXPORTED
-#endif // __GNUC__ >=3
