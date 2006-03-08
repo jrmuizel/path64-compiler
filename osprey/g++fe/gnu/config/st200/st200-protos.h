fbaa6c5604b06fca60c84d68dbf5eef7a230be5a
clarkes <clarkes@9f6548e3-d00b-0410-a34f-e49d3b37daf0>
2006-03-08 12:34:08 +0000

    [SC] Fix st200-protos.h files.
    * osprey/gccfe/gnu/config/st200/st200-protos.h: Make prototype
    declaration conditional on RTX_CODE and TREE_CODE.
    * osprey/g++fe/gnu/config/st200/st200-protos.h: Likewise.
    
    
    git-svn-id: https://codex.cro.st.com/svnroot/open64/trunk@8076 9f6548e3-d00b-0410-a34f-e49d3b37daf0

--- osprey/g++fe/gnu/config/st200/st200-protos.h
+++ osprey/g++fe/gnu/config/st200/st200-protos.h
@@ -21,23 +21,31 @@ Boston, MA 02111-1307, USA.  */
 #ifndef GCC_ST200_PROTOS_H
 #define GCC_ST200_PROTOS_H
 
-extern rtx  lx_function_arg               PARAMS ((CUMULATIVE_ARGS *, enum machine_mode, tree, int));
-extern int  lx_function_arg_partial_nregs PARAMS ((CUMULATIVE_ARGS, enum machine_mode, tree, int));
-extern void lx_function_arg_advance       PARAMS ((CUMULATIVE_ARGS *, enum machine_mode, tree, int named));
-extern tree lx_build_va_list              PARAMS ((void));
+#ifdef RTX_CODE
 extern rtx  lx_builtin_saveregs           PARAMS ((void));
-extern void lx_va_start                   PARAMS ((tree, rtx));
-
-extern int  lx_must_pass_in_stack     	  PARAMS ((enum machine_mode, tree));
-extern int  lx_direct_return              PARAMS ((void));
-
 extern int  lx_long_imm_operand           PARAMS ((rtx, enum machine_mode));
 extern int  lx_long_add_operand           PARAMS ((rtx, enum machine_mode));
-
-extern void lx_override_options           PARAMS ((void));
-extern int  lx_elimination_offset         PARAMS ((int, int));
 extern void lx_print_operand              PARAMS ((FILE *, rtx, int));
 extern enum reg_class lx_secondary_reload_class  PARAMS ((enum reg_class, enum machine_mode, rtx));
 extern void lx_reorg                      PARAMS ((rtx));
+
+#ifdef TREE_CODE
+extern rtx  lx_function_arg               PARAMS ((CUMULATIVE_ARGS *, enum machine_mode, tree, int));
+extern void lx_va_start                   PARAMS ((tree, rtx));
+extern rtx  lx_va_arg                     PARAMS ((tree, tree));
+
+#endif /* TREE_CODE */
+#endif /* RTX_CODE */
+
+#ifdef TREE_CODE
+extern int  lx_function_arg_partial_nregs PARAMS ((CUMULATIVE_ARGS, enum machine_mode, tree, int));
+extern void lx_function_arg_advance       PARAMS ((CUMULATIVE_ARGS *, enum machine_mode, tree, int named));
+extern tree lx_build_va_list              PARAMS ((void));
+extern int  lx_must_pass_in_stack     	  PARAMS ((enum machine_mode, tree));
+#endif /* TREE_CODE */
+
+extern int  lx_direct_return              PARAMS ((void));
+extern void lx_override_options           PARAMS ((void));
+extern int  lx_elimination_offset         PARAMS ((int, int));
 
 #endif /* GCC_ST200_PROTOS_H */
