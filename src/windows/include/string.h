6b59bb7cff8cb4221110a66af358dcc4073e96a8
unknown <Timo Kreuzer@.(none)>
2010-06-18 03:23:43 +0200

    Add some missing definitions to wrapper headers

--- src/windows/include/string.h
+++ src/windows/include/string.h
@@ -1,4 +1,7 @@
 
 #include_next <string.h>
 
-char *strtok_r(char *s1, const char *s2, char **s3);
+char *__strtok_r(char *s1, const char *s2, char **s3);
+#define strtok_r __strtok_r
+
+char * __rawmemchr(const char * s, int c_in);
--- src/windows/include/sys/stat.h
+++ src/windows/include/sys/stat.h
@@ -1,9 +1,9 @@
 #include_next <sys/stat.h>
 
-#define _S_IXGRP    _S_IXUSR
-#define _S_IXOTH    _S_IXUSR
-#define S_IXGRP     _S_IXGRP
-#define S_IXOTH     _S_IXOTH
+#define _STAT_VER 0
+
+#define S_IXGRP     _S_IXUSR
+#define S_IXOTH     _S_IXUSR
 
 #include <io.h>
 #define mkdir(path, mode) _mkdir(path)
