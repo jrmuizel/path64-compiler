e246f82a5cbb7a6d26d097b8e3525504b7a3d3b1
Timo Kreuzer <timo.kreuzer@web.de>
2010-07-05 23:21:45 +0200

    Fix gen_version.cmd to generate the username, date and a path, which is not really the root, but the current path

--- src/driver/gen_version.cmd
+++ src/driver/gen_version.cmd
@@ -1,7 +1,8 @@
 @echo off
+set DIR=%CD:\=\\%
 echo #include "version.h"
 echo const char *const cset_id = "";
-echo const char *const build_root = "";
+echo const char *const build_root = "%DIR%";
 echo const char *const build_host = "";
-echo const char *const build_user = "";
-echo const char *const build_date = "";
+echo const char *const build_user = "%USERNAME%";
+echo const char *const build_date = "%DATE%";
