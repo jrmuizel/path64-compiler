5bee523c6659e7c39a3ff2e29b8b7ee1984a9a92
Timo Kreuzer <timo.kreuzer@web.de>
2010-07-06 15:30:55 +0200

    Add WIFEXITED and WEXITSTATUS macros

--- src/windows/include/sys/wait.h
+++ src/windows/include/sys/wait.h
@@ -1,5 +1,16 @@
 
 #include <sys/types.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 pid_t wait(int *stat_loc);
 pid_t waitpid(pid_t pid, int *stat_loc, int options);
+
+#define WIFEXITED(s) ((s) != 259) /* STILL_ACTIVE */
+#define WEXITSTATUS(s) (s)
+
+#ifdef __cplusplus
+}
+#endif
