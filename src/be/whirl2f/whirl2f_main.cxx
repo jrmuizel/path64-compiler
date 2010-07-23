33bf0a6f58034ec50b8f1403b917d96e78861b76
Joerg Sonnenberger <joerg@bec.de>
2010-07-23 21:20:10 +0200

    Always use limits.h for PATH_MAX

--- src/be/whirl2f/whirl2f_main.cxx
+++ src/be/whirl2f/whirl2f_main.cxx
@@ -35,13 +35,7 @@
 #include <stdlib.h>		    /* for getenv() */
 #include <unistd.h>		    /* for execv() */
 #include <string.h>		    /* for strcpy(),etc. */
-#if defined(__FreeBSD__)
-#include <sys/syslimits.h>          
-#elif defined(__sun)
-#include <limits.h>
-#else
-#include <linux/limits.h>	    /* for PATH_MAX */
-#endif
+#include <limits.h>                 /* for PATH_MAX, etc. */
 #include <errno.h>		    /* for errno, sys_errlist[] */
 #include <cmplrs/rcodes.h>
 #include "defs.h"
