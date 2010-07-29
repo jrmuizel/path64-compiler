282f10ca747ef20a1ffe0d5d771749d788a1a68d
Ake Sandgren <ake@hpc2n.umu.se>
2010-07-29 12:53:54 +0200

    This is NOT a commit that should go into master!! It is just a proof-of-concept on what needs to be fixed for FreeBSD in libmpath and math error handling. FreeBSD is totally lacking the matherr function, struct exception and the defines used.

--- src/libmpath/amd/lib_version.h
+++ src/libmpath/amd/lib_version.h
@@ -12,6 +12,29 @@
 #define PATH64_LIB_VERSION_SVID (_LIB_VERSION == _SVID_)
 #define PATH64_LIB_VERSION_POSIX (_LIB_VERSION == _POSIX_)
 
+#elif defined(__FreeBSD__)
+
+#define PATH64_LIB_VERSION_SVID 0
+#define PATH64_LIB_VERSION_POSIX 1
+
+struct exception {
+    int type;
+    char *name;
+    double arg1;
+    double arg2;
+    double retval;
+};
+
+/* Types of exceptions in the `type' field.  */
+# define DOMAIN         1
+# define SING           2
+# define OVERFLOW       3
+# define UNDERFLOW      4
+# define TLOSS          5
+# define PLOSS          6
+
+#define matherr(x) (1)
+
 #elif defined(__NetBSD__)
 
 #define PATH64_LIB_VERSION_SVID 0
