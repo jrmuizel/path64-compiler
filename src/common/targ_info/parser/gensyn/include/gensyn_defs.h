5f9033cdebc6e3c0a19603cc6912bfd175802573
Joerg Sonnenberger <joerg@bec.de>
2010-08-03 18:39:45 +0000

    Use standard types.

--- src/common/targ_info/parser/gensyn/include/gensyn_defs.h
+++ src/common/targ_info/parser/gensyn/include/gensyn_defs.h
@@ -5,27 +5,16 @@
 #ifndef __DEFS_H__
 #define __DEFS_H__
 
-#if defined(__sun)
-typedef unsigned short uint16;               /**< unsigned 16-bit word */
-typedef short int16;                         /**< signed 16-bit word */
-typedef long int32;                          /**< signed 32-bit word */
-typedef unsigned long uint32;                /**< unsigned 32-bit word */
-typedef long long int64;                     /**< signed 64-bit word */
-typedef unsigned long long uint64;           /**< unsigned 64-bit word */
-#elif defined(linux) || defined(__MINGW32__)
-typedef unsigned short uint16;               /**< unsigned 16-bit word */
-typedef short int16;                         /**< signed 16-bit word */
-typedef long int32;                          /**< signed 32-bit word */
-typedef unsigned long uint32;                /**< unsigned 32-bit word */
-typedef long long int64;                     /**< signed 64-bit word */
-typedef unsigned long long uint64;           /**< unsigned 64-bit word */
-#elif defined(WIN32) || defined(__CYGWIN__)
-typedef unsigned short uint16;               /**< unsigned 16-bit word */
-typedef short int16;                         /**< signed 16-bit word */
-typedef long int32;                          /**< signed 32-bit word */
-typedef unsigned long uint32;                /**< unsigned 32-bit word */
-typedef __int64 int64;                       /**< signed 64-bit word */
-typedef unsigned __int64 uint64;             /**< unsigned 64-bit word */
+#include <stdint.h>
+
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+#if defined(WIN32) || defined(__CYGWIN__)
 #ifdef strcasecmp
    #undef strcasecmp
 #endif
@@ -35,9 +24,7 @@ typedef unsigned __int64 uint64;             /**< unsigned 64-bit word */
    #undef snprintf
 #endif
 #define snprintf _snprintf
-#else
-#error "Unknown target, please adapt gensyn_defs.h to your platform"
-#endif
+#endif // WIN32 || __CYGWIN__
 
 #define MAX_SEM_LEN 256 /**< Maximum identifier length (in character) */
 #endif /* __DEFS_H__ */
