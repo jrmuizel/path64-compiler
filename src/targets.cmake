7e0c0524abd0a8ae5bb84756717381c66acd503d
Joerg Sonnenberger <joerg@bec.de>
2010-08-03 19:41:23 +0000

    Enable x8664_32 and x8664_64 targets to x86_32 and x86_64

--- src/targets.cmake
+++ src/targets.cmake
@@ -1,7 +1,7 @@
 # message() string listing allowed target architectures.
 set(_PATH64_SUPPORTED_TARGETS_STRING "Supported architectures are:
-  x8664_32
-  x8664_64
+  x86_32
+  x86_64
   mips_32
   mips_64
   rsk6_32
@@ -11,15 +11,15 @@ set(_PATH64_SUPPORTED_TARGETS_STRING "Supported architectures are:
 # Target information table, keyed by entries of PATH64_ENABLE_TARGETS.
 # Reference table entries with ${_PATH64_TARGET_ARCH_${arch}}.
 
-set(_PATH64_TARGET_ARCH_x8664_32 x8664)
-set(_PATH64_TARGET_FLAG_x8664_32 -m32)
-set(_PATH64_TARGET_BITS_x8664_32 32)
-set(_PATH64_TARGET_ABI_x8664_32 ABI_M32)
+set(_PATH64_TARGET_ARCH_x86_32 x8664)
+set(_PATH64_TARGET_FLAG_x86_32 -m32)
+set(_PATH64_TARGET_BITS_x86_32 32)
+set(_PATH64_TARGET_ABI_x86_32 ABI_M32)
 
-set(_PATH64_TARGET_ARCH_x8664_64 x8664)
-set(_PATH64_TARGET_FLAG_x8664_64 -m64)
-set(_PATH64_TARGET_BITS_x8664_64 64)
-set(_PATH64_TARGET_ABI_x8664_64 ABI_M64)
+set(_PATH64_TARGET_ARCH_x86_64 x8664)
+set(_PATH64_TARGET_FLAG_x86_64 -m64)
+set(_PATH64_TARGET_BITS_x86_64 64)
+set(_PATH64_TARGET_ABI_x86_64 ABI_M64)
 
 set(_PATH64_TARGET_ARCH_mips_32 mips)
 set(_PATH64_TARGET_FLAG_mips_32 -n32)
@@ -105,7 +105,7 @@ endfunction()
 # Returns target for host system
 function(path64_get_host_target res_var)
     # TODO: find way how to get target for host system
-    set(${res_var} "x8664_64" PARENT_SCOPE)
+    set(${res_var} "x86_64" PARENT_SCOPE)
 endfunction()
 
 
