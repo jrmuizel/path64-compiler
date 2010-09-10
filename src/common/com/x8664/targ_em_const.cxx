2599c8c3c128e7c686e3baed41db443d68d92d5b
Alexander Esilevich <aesilevich@pathscale.com>
2010-09-10 02:35:56 +0000

    Quotes were added to avoid trailing slashes in emitted code

--- src/common/com/x8664/targ_em_const.cxx
+++ src/common/com/x8664/targ_em_const.cxx
@@ -153,14 +153,14 @@ Targ_Emit_String ( FILE *fl, char *str, INT32 len, INTSC loc )
 	++n_on_line;
 	if ( n_on_line == MAX_LEN ) {
 	    *dptr = '\0';
-	    fprintf(fl, "\t%s %s", ASM_CMNT, dbuf);
+	    fprintf(fl, "\t%s \"%s\"", ASM_CMNT, dbuf);
 	    dptr = dbuf;
 	    fprintf ( fl, "\n" );
 	    n_on_line = 0;
 	}
     }
     *dptr = '\0';
-    fprintf(fl, "\t%s %s", ASM_CMNT, dbuf);
+    fprintf(fl, "\t%s \"%s\"", ASM_CMNT, dbuf);
     dptr = dbuf;
     fprintf(fl, "\n");
 
