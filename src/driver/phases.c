041d402d7ca49f3142ddbc3de224c75af86bb049
Alexander Esilevich <aesilevich@pathscale.com>
2010-09-11 13:30:33 +0000

    Fix for pathas debugging options in driver

--- src/driver/phases.c
+++ src/driver/phases.c
@@ -1610,13 +1610,17 @@ add_file_args (string_list_t *args, phases_t index)
 	case P_as:
 	case P_gas:
 #ifdef KEY
+#ifdef PATH64_ENABLE_PATHAS
+		if (glevel >= 2) {
+		  add_string(args, "-g");
+		  add_string(args, "dwarf2pass");
+		}
+#else // !PATH64_ENABLE_PATHAS
 		if (source_lang == L_as &&
 		    glevel >= 2) {
 		  add_string(args, "-g");	// bug 5990
-#ifdef PATH64_ENABLE_PATHAS
-		  add_string(args, "dwarf2pass");	// bug 5990
-#endif // !PATH64_ENABLE_PATHAS
 		}
+#endif // !PATH64_ENABLE_PATHAS
 #endif
 		if (dashdash_flag)
 		  add_string(args,"--");
