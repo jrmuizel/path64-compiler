222137a64fb19c6484c129980eca45cf031d6612
/C <asura@m4400.(none)>
2011-01-12 12:38:48 +0700

    Turn back on all optimizations

--- src/be/be/driver.cxx
+++ src/be/be/driver.cxx
@@ -320,7 +320,7 @@ static Output_File *ir_output = 0;
 // options stack for PU and region level pragmas
 static OPTIONS_STACK *Options_Stack;
 
-static BOOL reset_opt_level = TRUE;
+static BOOL reset_opt_level = FALSE;
 static struct ALIAS_MANAGER *alias_mgr = NULL;
 
 static BOOL Run_Distr_Array = FALSE;
