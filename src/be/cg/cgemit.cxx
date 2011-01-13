f164df9f408d0fb6cf18bfeb56a39de16ec854e7
Ake Sandgren <ake@hpc2n.umu.se>
2011-01-13 14:19:33 +0100

    Revert "volatile asm regression fixed"
    This reverts commit 3efb1dbbb3b75c5ae3fc412b8aeed5818866de2a.
    
    Preparing to revert commit 00fd00d3602c106735f7357b45ce4db6a4bf9fb4

--- src/be/cg/cgemit.cxx
+++ src/be/cg/cgemit.cxx
@@ -10829,9 +10829,9 @@ Modify_Asm_String (char* asm_string, UINT32 position, bool memory,
 #ifdef KEY
 		   TN* offset_tn,  /* for memory opnd */
 #endif
-		   TN* tn, BB *bb, int opvolatile)
+		   TN* tn, BB *bb)
 {
-   if( tn->size == 8 && !opvolatile){
+   if( tn->size == 8 ){
     char pattern[5]; 
      strcpy(pattern, "movb");
     asm_string =  Replace_Substring(asm_string, pattern, "movq");
@@ -11327,7 +11327,7 @@ Generate_Asm_String (OP* asm_op, BB *bb)
 #ifdef KEY
 				   NULL,
 #endif
-                                   OP_result(asm_op, i), bb, OP_volatile(asm_op));
+                                   OP_result(asm_op, i), bb);
   }
 
   for (i = 0; i < OP_opnds(asm_op); i++) {
@@ -11337,7 +11337,7 @@ Generate_Asm_String (OP* asm_op, BB *bb)
 #ifdef KEY
 				   (TN*)ASM_OP_opnd_offset(asm_info)[i],
 #endif
-                                   OP_opnd(asm_op, i), bb, OP_volatile(asm_op));
+                                   OP_opnd(asm_op, i), bb);
   }
 
 #ifndef TARG_X8664
