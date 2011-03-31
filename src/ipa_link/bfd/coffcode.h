a64c11b543134601095316deb9be6d0ab0ee7088
Alexander Esilevich <aesilevich@pathscale.com>
2011-03-31 09:27:29 +0700

    TARG_ST code was removed from ipa_link

--- src/ipa_link/bfd/coffcode.h
+++ src/ipa_link/bfd/coffcode.h
@@ -1832,12 +1832,10 @@ coff_mkobject_hook (bfd * abfd,
       xcoff->maxstack = internal_a->o_maxstack;
     }
 #endif
-#ifndef TARG_ST
 #ifdef ARM
   /* Set the flags field from the COFF header read in.  */
   if (! _bfd_coff_arm_set_private_flags (abfd, internal_f->f_flags))
     coff->flags = 0;
-#endif
 #endif
 #ifdef COFF_WITH_PE
   /* FIXME: I'm not sure this is ever executed, since peicode.h
