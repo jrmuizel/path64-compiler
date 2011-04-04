a381178a0ad5a9fe90995c772f3a551ff92cb638
Alexander Esilevich <aesilevich@pathscale.com>
2011-04-04 13:44:08 +0700

    missing sources were added to comutil library

---
+++ src/common/util/string_case.h
@@ -0,0 +1,17 @@
+
+#ifndef STRING_CASE_H
+#define STRING_CASE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+char *string_tolower(char *str);
+char *string_toupper(char *str);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // STRING_CASE_H
+
