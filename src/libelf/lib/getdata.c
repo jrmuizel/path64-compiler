497b03c1ae42f8a8b3296b8b1c2f124210df50ae
Ake Sandgren <ake@hpc2n.umu.se>
2011-04-05 11:23:21 +0200

    Drop even more useless compares of 'unsigned int < 0'

--- src/libelf/lib/getdata.c
+++ src/libelf/lib/getdata.c
@@ -140,7 +140,7 @@ elf_getdata(Elf_Scn *scn, Elf_Data *data) {
 	    /* already cooked */
 	    return &sd->sd_data;
 	}
-	else if (scn->s_offset < 0 || scn->s_offset > elf->e_size) {
+	else if (scn->s_offset > elf->e_size) {
 	    seterr(ERROR_OUTSIDE);
 	}
 	else if (scn->s_type == SHT_NOBITS || !scn->s_size) {
