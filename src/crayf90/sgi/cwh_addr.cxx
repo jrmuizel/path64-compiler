jrmuizel/path64-compiler

commit 75178aead3dfb4f4a8d22dd1b6186b50ab40de9e

Fix call by value parameter used as actual arg.

src/crayf90/sgi/cwh_addr.cxx
@@ -1912,11 +1912,8 @@ cwh_addr_address_ST(ST * st, OFFSET_64 off, TY_IDX ty)
   switch (ST_sclass(st)){
   case SCLASS_FORMAL:
 
-    DevAssert((TY_kind(ty) == KIND_POINTER),("formal & non-pointer"));
-
 #ifdef KEY /* Bug 14150 */
-    if (ST_is_value_parm(st) && !ST_auxst_is_rslt_tmp(st) &&
-       !PROC_BY_VALUE(ty)) {
+    if (ST_is_value_parm(st) && !ST_auxst_is_rslt_tmp(st)) {
        wn = cwh_addr_lda(st,off,ty);
        return wn;
     }

commit f023badca8f2176b6a45b3d1e009da12283484c3

Fix regression caused by commit 75178aead3dfb4f4a8d22dd1b6186b50ab40de9e

src/crayf90/sgi/cwh_addr.cxx
@@ -1913,7 +1913,8 @@ cwh_addr_address_ST(ST * st, OFFSET_64 off, TY_IDX ty)
   case SCLASS_FORMAL:
 
 #ifdef KEY /* Bug 14150 */
-    if (ST_is_value_parm(st) && !ST_auxst_is_rslt_tmp(st)) {
+    if (ST_is_value_parm(st) && !ST_auxst_is_rslt_tmp(st) &&
+        !((TY_kind(ty) == KIND_POINTER) && (TY_kind(TY_pointed(ty)) == KIND_FUNCTION))) {
        wn = cwh_addr_lda(st,off,ty);
        return wn;
     }